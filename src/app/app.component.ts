dde0bb8d74c3b109ec9500dadfb613c6e74958b4
RaKo <kovacic@strato.de>
2017-06-20 15:45:05 +0200

    initialer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+  <h1>
+  {{title}}
+  </h1>
+  <app-input></app-input>
+`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  private title: string = 'app works!';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { FoobarComponent } from './foobar/foobar.component';
+import { InputComponent } from './input/input.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FoobarComponent,
+    InputComponent
   ],
   imports: [
     BrowserModule,
